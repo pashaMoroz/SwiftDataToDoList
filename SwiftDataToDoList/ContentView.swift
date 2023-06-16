9f5170728b2a1bf2b206823740a2fdfffa517759
Moroz Pavlo <pavlo@sidekickinteractive.com>
2023-06-16 09:21:14 -0400

    Initial Commit

---
+++ SwiftDataToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataToDoList
+//
+//  Created by Moroz Pavlo on 2023-06-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataToDoList/SwiftDataToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataToDoListApp.swift
+//  SwiftDataToDoList
+//
+//  Created by Moroz Pavlo on 2023-06-16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
